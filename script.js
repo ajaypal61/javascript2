e1d9a027c675b183fac596ce3c90caf8f05ea5dd
ajaypal61 <ajaypal2987@gmail.com>
2022-04-14 09:04:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,341 @@
+// console.log("this is ajay")
+
+
+// statement and typecasting
+
+
+// statement
+
+
+
+// document.write("this is simple man");
+
+// function sp(params) {
+// document.write("this is ajay")
+// }
+
+// sp();
+
+// sp yaha naam hai is ki jagah kuch bhi de sakte
+
+
+
+// ham var yaa let kuch bhi use kr sakte
+
+
+
+
+// typecasting
+
+
+// let a = 85;
+// let b = 3.25;
+// let c = "india";
+// let d = true;
+// let e = ["india", "pakistan", "nepal", "bagladesh"];
+
+
+
+// document.write(typeof (a));
+// document.write("<br>")
+// document.write(typeof (b));
+// document.write("<br>")
+// document.write(typeof (c));
+// document.write("<br>")
+// document.write(typeof (d));
+// document.write("<br>")
+// document.write(typeof (e));
+// document.write("<br>")
+
+// let f = String(a);
+// string ek text hota hai
+// document.write(40 + f);
+
+
+
+
+// datatypes & variables
+
+
+
+
+// data types in js
+
+
+
+// primitive hote hai
+// 1 number = 1,2....
+// 2 string = text
+// 3 null = khali
+// 4 booleon = true, false
+// 5 undifind = bahut
+
+
+// ye data type hote
+    
+    
+// let a = 51;
+// document.write(typeof (a));
+// document.write("<br>");
+
+// let b = "51";
+// document.write(typeof (b));
+// document.write("<br>");
+
+// let c = null;
+// document.write(typeof (c));
+// document.write("<br>");
+
+// let d = true;
+// document.write(typeof (d));
+// document.write("<br>");
+
+// let e;
+// document.write(typeof (e));
+// document.write("<br>");
+
+
+
+// non-primitive js hote hai
+
+
+// let brand = [ "maruti", "hondai", "skoda", "mahindra"];
+// document.write(typeof (brand));
+// document.write("<br>")
+// alert("I purchased " + brand[1]);
+
+
+
+// let mycar = { brand: "maruti", model: "swift", variant: "VXI Optional", year: 2021 }
+// document.write(typeof (mycar));
+// document.write("<br>");
+// alert("I Purchased Swift 2021 and its variant is " + mycar.variant);
+
+
+
+
+
+
+// input & output in java script
+
+
+
+
+// let val = prompt("Enter a Number");
+// document.write("You entered " + val);
+// document.write("<br>");
+// document.write("You entered " + val);
+// console.log("You entered " + val);
+
+
+
+
+
+// operators in java script
+
+
+
+
+// 5 type ke hote hai ye
+
+
+// 1 arithmetic operator
+
+
+// let a = 55;
+// let b = 45;
+
+// let c = (a + b);
+// document.write("the addition is " + c);
+// document.write("<br>");
+// let d = (a - b);
+// document.write("the subtraction is " + d);
+// document.write("<br>");
+// let e = (a * b);
+// document.write("the multiplication is " + e);
+// document.write("<br>");
+// let f = (a / b);
+// document.write("the divide is " + f);
+// document.write("<br>");
+// let g = (a % b);
+// document.write("the modulis is " + g);
+// document.write("<br>");
+
+
+
+
+// 2 comparison operators
+
+
+// let a = 55;
+// let b = 45;
+
+// let cc = (a = b);
+// document.write("Equality btw a and b is " + cc);
+// document.write("<br>");
+
+// let cd = (a < b);
+// document.write("Lass than btw a and b is " + cd);
+// document.write("<br>");
+
+// let ce = (a > b);
+// document.write("greater than btw a and b is " + ce);
+// document.write("<br>");
+
+// let cr = (a <= b);
+// document.write("less equal btw a and b is " + cr);
+// document.write("<br>");
+
+// let cf = (a >= b);
+// document.write("greater equal btw a and b is " + cf);
+// document.write("<br>");
+
+
+
+// 3 logical operatar
+
+
+// let a = 55;
+// let b = 45;
+
+// if (!(a == 4 && b == 6)) {
+//     document.write("correct");
+// } else {
+//     document.write("incorrect");
+// }
+// document.write("<br>");
+
+
+// if (a == 4 && b == 6) {
+//     document.write("correct");
+// } else {
+//     document.write("incorrect");
+// }
+// document.write("<br>");
+
+
+// if (!(a == 4 || b == 6)) {
+//     document.write("correct");
+// } else {
+//     document.write("incorrect");
+// }
+// document.write("<br>");
+
+
+// 4 bitwise operator
+
+
+// let a = 55;
+// let b = 45;
+
+// let c = (a | b);
+// document.write("OR Result is " + c);
+// document.write("<br>");
+
+
+
+// 5 assigment operators
+
+
+// let a = 55;
+// let b = 45;
+
+// let c = (a += b);
+// document.write("Result is " + c);
+
+
+
+
+
+
+// if-else in java script
+
+
+
+
+// let age = prompt("Enter your age");
+
+// if (age < 18) {
+//     document.write("You are not eligible for vote");
+// } else if (age >= 18 && age <= 95) {
+//     document.write("You are eligible for vote");
+// } else {
+//     document.write("You Input Invalid datails");
+// }
+
+
+
+// switch-case statement in java script
+
+
+
+
+// document.write("Press 1 for spelling");
+// document.write("<br>");
+
+// document.write("Press 2 for spelling");
+// document.write("<br>");
+
+// document.write("Press 3 for spelling");
+// document.write("<br>");
+
+// document.write("Press 4 for spelling");
+// document.write("<br>");
+
+// document.write("Press 5 for spelling");
+// document.write("<br>");
+
+
+// let usr = 4;
+
+// switch (usr) {
+//     case 1:
+//         document.write("ONE")
+//         break;
+//     case 2:
+//         document.write("TWO")
+//         break;
+//     case 3:
+//         document.write("THREE")
+//         break;
+//     case 4:
+//         document.write("FOUR")
+//         break;
+//     case 5:
+//         document.write("FIVE")
+//         break;
+
+//     default:
+//         document.write("Invalid Input");
+//         break;
+// }
+
+
+
+
+// loops in java script
+
+
+
+// 4 type ke hote hai
+// 1 for loop
+// 2 for in
+// 3 while
+// 4 do while
+
+
+
+
+// functions in java script
+
+
+
+
+// let & constant in java script
+
+
+
+
+
+
+
---
+++ src.js
@@ -0,0 +1,32 @@
+// const arrayquotes = [
+//     {
+//         'author': 'John Cena',
+//         'quote' : 'Never Give Up',
+//     },
+//     {
+//         'author': 'John Anthony Cena',
+//         'quote' : 'Hustle Loyalty Respect',
+//     },
+// ];
+// function genquote() {
+//     const random = Number.parseInt(Math.random() * arrayquotes.length);
+//     document.querySelector('#qt').textContent = '\"${arrayquotes[random].quote}\"';
+//     document.querySelector('#at').textContent = '--${arrayquotes[random].author}';
+// }
+
+
+// const arrayquotes = [
+//     {
+//         'author': 'John Cena',
+//         'quote': 'Never Give Up',
+//     },
+//     {
+//         'author': 'John Anthony Cena',
+//         'quote': 'Never Give Up',
+//     },
+// ];
+// function genquote() {
+//     const random = Number.parseInt(Math.random() * arrayquotes.length);
+//     document.querySelector('#qt').textContent = '\"{arrayofQuotes[random].quote}\"';
+//     document.querySelector('#at').textContent = '--${arrayofquotes[random].auothe}';
+// }
